0f096fe4600c3b82c1a116e4a3d8edd0d5c0f654
leahdb <hdeiblea72@gmail.com>
2023-01-06 15:21:28 +0200

    homepage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.scss';
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from './Pages/Home/Home';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 
 
  export default function App() {
